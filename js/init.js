9d78240971303a4f806a23a9d359ca11b5ec929a
Garrett Gu <garrettgu777@gmail.com>
2016-12-23 21:13:35 -0500

    Added badge to image for links

--- js/init.js
+++ js/init.js
@@ -1,4 +1,13 @@
 
 $(document).ready(function(){
-   $('.parallax').parallax();
+   $('.unhovered1').on("mouseenter",function(){
+      $('.unhovered1').css("opacity",0);
+      $('.hovered1').css("opacity",1);
+      $('.hovered1').css("transform","scale(1,1)");
+   });
+   $('.hovered1').on("mouseleave",function(){
+      $('.unhovered1').css("opacity",1);
+      $('.hovered1').css("opacity",0);
+      $('.hovered1').css("transform","scale(0,0)");
+   });
 });
