b1448838ad804092adf835f2e87085a689b287df
Garrett Gu <garrettgu777@gmail.com>
2016-12-23 22:49:21 -0500

    added outreach and about bubbles and content

--- js/init.js
+++ js/init.js
@@ -1,13 +1,29 @@
 
 $(document).ready(function(){
-   $('.unhovered1').on("mouseenter",function(){
-      $('.unhovered1').css("opacity",0);
-      $('.hovered1').css("opacity",1);
-      $('.hovered1').css("transform","scale(1,1)");
+   for(var i = 1; i <= 2; i++){
+      $('.unhovered'+i).on("mouseenter",function(){
+         $(this).css("opacity",0);
+         $(this).next().css("opacity",1);
+         $(this).next().css("transform","scale(1,1)");
+      });
+      $('.hovered'+i).on("mouseleave",function(){
+         $(this).prev().css("opacity",1);
+         $(this).css("opacity",0);
+         $(this).css("transform","scale(0,0)");
+      });
+   }
+   /*$('.unhovered2').on("mouseenter",function(){
+      $('.unhovered2').css("opacity",0);
+      $('.hovered2').css("opacity",1);
+      $('.hovered2').css("transform","scale(1,1)");
    });
-   $('.hovered1').on("mouseleave",function(){
-      $('.unhovered1').css("opacity",1);
-      $('.hovered1').css("opacity",0);
-      $('.hovered1').css("transform","scale(0,0)");
-   });
+   $('.hovered2').on("mouseleave",function(){
+      $('.unhovered2').css("opacity",1);
+      $('.hovered2').css("opacity",0);
+      $('.hovered2').css("transform","scale(0,0)");
+   });*/
 });
+
+function scrollTo(headerName){
+   $("html, body").animate({ scrollTop: $("."+headerName+"-header").position().top }, 500, "swing");
+}
