dd8a1ccc9449933efc471bdbf49a16b888bf16e5
Garrett Gu <garrettgu777@gmail.com>
2016-12-24 20:59:15 -0500

    made photos smaller, moved image title up, added Josiah

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 
 $(window).on("load", function(){
    $(".badge.unhovered").css("transform","scale(1,1)");
-   $(".image-title").css({"opacity": 1, "top": "44%"});
+   $(".image-title").css({"opacity": 1, "top": "0%"});
 
    $('.badge-container').on("mouseenter",function(){
       $(this).children(".unhovered").css("opacity",0);
