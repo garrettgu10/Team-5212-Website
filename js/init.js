a72750b29e53ed7ce11cac8c86a790123cde0d99
Garrett Gu <garrettgu777@gmail.com>
2016-12-26 23:35:21 -0600

    Added img-circle click-to-enlarge, separated rows into divs, added Andrew photo

--- js/init.js
+++ js/init.js
@@ -4,12 +4,12 @@ $(window).on("load", function(){
    $(".image-title").css({"opacity": 1, "top": "0%"});
    $(".button-collapse").sideNav();
 
-   $('.badge-container').on("mouseenter",function(){
+   $(".badge-container").on("mouseenter",function(){
       $(this).children(".unhovered").css("opacity",0);
       $(this).children(".hovered").css("opacity",1);
       $(this).children(".hovered").css("transform","scale(1.5,1.5)");
    });
-   $('.badge-container').on("mouseleave",function(){
+   $(".badge-container").on("mouseleave",function(){
       $(this).children(".unhovered").css("opacity",1);
       $(this).children(".hovered").css("opacity",0);
       $(this).children(".hovered").css("transform","scale(0,0)");
@@ -28,8 +28,33 @@ $(window).on("load", function(){
          if($(elm).attr("class").split(" ")[0] == "about")
             setActiveNav(null);
       }
-   })
+   });
+
+   $(".img-circle").on("click", function(){
+      var clone = $(this).clone();
+      var cloneDiameter = min($(window).height(),$(window).width())*0.8;
+      clone.css({
+         "position": "relative",
+         "width": cloneDiameter+"px", "height": cloneDiameter+"px",
+         "left": "50%", "top": "50%",
+         "margin-left": -(cloneDiameter/2)+"px", "margin-top": -(cloneDiameter/2)+"px",
+         "cursor": "zoom-out"
+      });
+      $(".shade").css("display", "inline");
+      $(".shade").append(clone);
+      $(".shade").animate({"opacity":1}, 500, "swing");
+   });
+   $(".shade").on("click", function(){
+      $(this).animate({"opacity":0}, 500, "swing", function(){
+         $(this).children(".img-circle").remove();
+         $(this).css("display","none");
+      });
+   });
 });
+
+function min(a, b){
+   return a<b? a:b;
+}
 
 function scrollTo(headerName){
    $("html, body").animate({ scrollTop: $("."+headerName+".header").position().top-50 }, 500, "swing");
